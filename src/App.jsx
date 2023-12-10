c9f544206bd5bdbbbebbbae63e8ac83972267dbf
DavitTcharbadze <153432509+DavitTcharbadze@users.noreply.github.com>
2023-12-10 20:41:53 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
- import React, { useState } from 'react';
+import React, { useState } from 'react';
+import classes from './modules/App.module.scss';
+ 
+const App = () => {
+    const [currentInput, setCurrentInput] = useState('');
+    const [firstColor, SetFirstColor] = useState(false);
+    const [secondColor, SetSecondColor] = useState(false);
+    const [thirdColor, SetThirdColor] = useState(false);
 
- const Calculator = () => {
-   const [currentInput, setCurrentInput] = useState('');
-
+    const FirstClickHandler = () => {
+        SetFirstColor(true);
+        SetSecondColor(false);
+        SetThirdColor(false);
+      }
+      const SecondClickHandler = () => {
+        SetSecondColor(true);
+        SetThirdColor(false);
+        SetFirstColor(false);
+      }
+      const ThirdClickHandler = () => {
+        SetThirdColor(true);
+        SetFirstColor(false);
+        SetSecondColor(false);
+      }
+      
    const appendNumber = (number) => {
      setCurrentInput(currentInput + number);
    };
@@ -28,7 +48,8 @@
    };
 
    return (
-     <div id="calculator">
+    <>
+     <div className={classes['main-calculator']} id="calculator">
        <input type="text" value={currentInput || '0'} readOnly />
        <br />
        <button onClick={() => appendNumber('7')}>7</button>
@@ -51,11 +72,18 @@
        <button onClick={calculate}>=</button>
        <button onClick={clearDisplay}>C</button>
        <button onClick={deleteLast}>DEL</button>
+       <div className={`${classes['first-mode']} ${firstColor === true && classes['first-mode']} ${secondColor === true && classes['second-mode']} ${thirdColor === true && classes['third-mode']}`}>
+      <h2 className={classes['head-line']}>THEME</h2>
+      <div className={classes['main-box']}>
+        <button onClick={FirstClickHandler} className={`${classes['first-button']}`}></button>
+        <button onClick={SecondClickHandler} className={`${classes['second-button']}`}></button>
+        <button onClick={ThirdClickHandler} className={`${classes['third-button']}`}></button>
+      </div>
+    </div>
      </div>
+     
+     </>
    );
  };
 
- export default Calculator;
-
-
-
+ export default App;
