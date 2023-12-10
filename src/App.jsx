d211895edf14ac4f8bb696708b4c2e172f86969a
only_lashaa <126561350+lashadezli@users.noreply.github.com>
2023-12-10 23:52:24 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,71 @@
 import React, { useState } from 'react';
-import classes from './modules/App.module.scss';
- 
+import classes from '../modules/App.module.scss';
+
 const App = () => {
-    const [currentInput, setCurrentInput] = useState('');
-    const [firstColor, SetFirstColor] = useState(false);
-    const [secondColor, SetSecondColor] = useState(false);
-    const [thirdColor, SetThirdColor] = useState(false);
+  const [currentInput, setCurrentInput] = useState('');
+  const [theme, setTheme] = useState('default');
 
-    const FirstClickHandler = () => {
-        SetFirstColor(true);
-        SetSecondColor(false);
-        SetThirdColor(false);
-      }
-      const SecondClickHandler = () => {
-        SetSecondColor(true);
-        SetThirdColor(false);
-        SetFirstColor(false);
-      }
-      const ThirdClickHandler = () => {
-        SetThirdColor(true);
-        SetFirstColor(false);
-        SetSecondColor(false);
-      }
-      
-   const appendNumber = (number) => {
-     setCurrentInput(currentInput + number);
-   };
+  const changeTheme = (newTheme) => {
+    setTheme(newTheme);
+  };
 
-   const setOperatorHandler = (operator) => {
-     setCurrentInput(currentInput + operator);
-   };
+  const appendNumber = (number) => {
+    setCurrentInput(currentInput + number);
+  };
 
-   const calculate = () => {
-     try {
-       setCurrentInput(eval(currentInput).toString());
-     } catch (error) {
-       setCurrentInput('Error');
-     }
-   };
+  const setOperatorHandler = (operator) => {
+    setCurrentInput(currentInput + operator);
+  };
 
-   const clearDisplay = () => {
-     setCurrentInput('');
-   };
+  const calculate = () => {
+    try {
+      setCurrentInput(eval(currentInput).toString());
+    } catch (error) {
+      setCurrentInput('Error');
+    }
+  };
 
-   const deleteLast = () => {
-     setCurrentInput(currentInput.slice(0, -1));
-   };
+  const clearDisplay = () => {
+    setCurrentInput('');
+  };
 
-   return (
+  const deleteLast = () => {
+    setCurrentInput(currentInput.slice(0, -1));
+  };
+
+  return (
     <>
-     <div className={classes['main-calculator']} id="calculator">
-       <div className='calculator'>
-       <div className='toggle'>
-        <p>calc</p>
-        <div>toggle</div>
-       </div>
-       <div className='keypad'>
-        <input type="text" value={currentInput || '0'} readOnly />
-       </div>
-       <div className='container'>
-        <div className='btn-container'>
-         <button onClick={() => appendNumber('7')}>7</button>
-         <button onClick={() => appendNumber('8')}>8</button>
-         <button onClick={() => appendNumber('9')}>9</button>
-         <button className='delete' onClick={deleteLast}>DEL</button>
+      <div className={`${classes['main-calculator']} ${classes[theme]}`} id="calculator">
+        <div className={classes['theme-buttons']}>
+          <button onClick={() => changeTheme('default')} className={classes['theme-button-blue']}></button>
+          <button onClick={() => changeTheme('second')} className={classes['theme-button-gray']}></button>
+          <button onClick={() => changeTheme('third')} className={classes['theme-button-purple']}></button>
         </div>
-        <div className='btn-container'>
-         <button onClick={() => appendNumber('4')}>4</button>
-         <button onClick={() => appendNumber('5')}>5</button>
-         <button onClick={() => appendNumber('6')}>6</button>
-         <button onClick={() => setOperatorHandler('+')}>+</button>
-        </div>
-        <div className='btn-container'>
-         <button onClick={() => appendNumber('1')}>1</button>
-         <button onClick={() => appendNumber('2')}>2</button>
-         <button onClick={() => appendNumber('3')}>3</button>
-         <button onClick={() => setOperatorHandler('-')}>-</button>
-        </div>
-        <div className='btn-container'>
-         <button onClick={() => appendNumber('.')}>.</button>
-         <button onClick={() => appendNumber('0')}>0</button>
-         <button onClick={() => setOperatorHandler('/')}>/</button>
-         <button onClick={() => setOperatorHandler('*')}>x</button>
-        </div>
-        <div className='btn-container'>
-         <button className='reset' onClick={clearDisplay}>RESET</button>
-         <button className='calculate-btn' onClick={calculate}>=</button>
-        </div>
-       </div>
-       
-     </div>
-       <div className={`${classes['first-mode']} ${firstColor === true && classes['first-mode']} ${secondColor === true && classes['second-mode']} ${thirdColor === true && classes['third-mode']}`}>
-      <h2 className={classes['head-line']}>THEME</h2>
-      <div className={classes['main-box']}>
-        <button onClick={FirstClickHandler} className={`${classes['first-button']}`}></button>
-        <button onClick={SecondClickHandler} className={`${classes['second-button']}`}></button>
-        <button onClick={ThirdClickHandler} className={`${classes['third-button']}`}></button>
+            <input type="text" value={currentInput || '0'} className={classes['number-input']} readOnly />
+                <br />
+                    <button onClick={() => appendNumber('7')} className={classes['number-button']}>7</button>
+                    <button onClick={() => appendNumber('8')} className={classes['number-button']}>8</button>
+                    <button onClick={() => appendNumber('9')} className={classes['number-button']}>9</button>
+                    <button onClick={() => setOperatorHandler('+')}className={classes['operator-button']} >+</button>
+                    <br />
+                    <button onClick={() => appendNumber('4')} className={classes['number-button']}>4</button>
+                    <button onClick={() => appendNumber('5')} className={classes['number-button']}>5</button>
+                    <button onClick={() => appendNumber('6')} className={classes['number-button']}>6</button>
+                    <button onClick={() => setOperatorHandler('-')} className={classes['operator-button']}>-</button>
+                    <br />
+                    <button onClick={() => appendNumber('1')} className={classes['number-button']}>1</button>
+                    <button onClick={() => appendNumber('2')} className={classes['number-button']}>2</button>
+                    <button onClick={() => appendNumber('3')} className={classes['number-button']}>3</button>
+                    <button onClick={() => setOperatorHandler('*')} className={classes['operator-button']}>*</button>
+                    <br />
+                    <button onClick={() => appendNumber('0')} className={classes['number-button']}>0</button>
+                    <button onClick={() => setOperatorHandler('/')} className={classes['operator-button']}>/</button>
+                    <button onClick={calculate} className={classes['equals-button']}>=</button>
+                    <button onClick={clearDisplay} className={classes['reset-delete-button']}>C</button>
+                    <button onClick={deleteLast} className={classes['reset-delete-button']}>DEL</button>
       </div>
-    </div>
-     </div>
-     
-     </>
-   );
- };
+    </>
+  );
+};
 
- export default App;
+export default App;
